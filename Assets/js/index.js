b8b91eca79a016210f62a855c9e6e54006a77c08
Sean Conrad <sean.w.conrad@gmail.com>
2022-08-28 18:31:39 -0400

    index.html time blocks

---
+++ Assets/js/index.js
@@ -0,0 +1,13 @@
+var formattedDate = moment().format("dddd, MMM DD, YYYY");
+//var currentDay = $('#current-day');
+//var moment = moment();
+var timeBlocks = $("#timeblocks");
+
+
+
+
+
+
+$("#current-day").text(formattedDate);
+
+
