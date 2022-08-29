92a1b8fa86c8936b59fa7c2954a5aa3cb6dfe84f
Sean Conrad <sean.w.conrad@gmail.com>
2022-08-28 20:54:51 -0400

    index.js file updates

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,13 +1,54 @@
 var formattedDate = moment().format("dddd, MMM DD, YYYY");
 //var currentDay = $('#current-day');
 //var moment = moment();
-var timeBlocks = $("#timeblocks");
-
-
-
-
-
 
 $("#current-day").text(formattedDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time,text);
+    })
+
+    function timeTracker () {
+        var currentTime =  moment().hour();
+
+        $(".time-block").each(function() {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+                
+            } else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("current");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    //append info from local storage to the screen
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    timeTracker();
+        
+})
+
 
 
